42f7d79d8d4d33b258fffd346c5d7d59634b1c27
curlychessyts <152013300+curlychessyts@users.noreply.github.com>
2026-03-22 17:15:17 +0900

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,10 +54,6 @@ export default {
                             <div class="type-title-sm">FPS</div>
                             <p>{{ level.FPS }}</p>
                         </li>
-                        <li>
-                            <div class="type-title-sm">Method</div>
-                            <p>{{ level.Method }}</p>
-                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
